3e6d66c4149d174456bb00762bfa9a8db602f285
omerolkun <omerolkun2004@gmail.com>
2024-08-18 00:28:22 +0300

    add new line to top of the txt

--- find.py
+++ find.py
@@ -1,4 +1,5 @@
 import time
+import re
 import cloudscraper
 import sys
 import requests
@@ -50,7 +51,7 @@ for seller in result_urls:
         for single_row in rows:
             name = single_row.find('a')['title']
             price = single_row.find(class_="col-md-6 col-xs-12 no-padding text-right product-list-price").string
-            price_spl = price.split(',')
+            price_spl = re.split(r"[,.]", price)
             int_price = int(price_spl[0])
             tup = (seller_name, name, int_price)
             result_list.append(tup)
@@ -62,7 +63,7 @@ print("Total Executione time: ", (end_time - start_time) / 60, "minutes...")
 file_name = sys.argv[1]
 mail_message = "Subject: {} \n\n".format(sys.argv[1])
 file = open('/home/om/Documents/ankaradakisahafkitap/{}.txt'.format(file_name), 'w')
-result_message = "Toplam " + str(len(result_list)) + " kitap bulunmustur."
+result_message = "Toplam " + str(len(result_list)) + " kitap bulunmustur.\n\n"
 file.write(result_message)
 result_list.sort(key=lambda a: a[2])
 for obj in result_list:
